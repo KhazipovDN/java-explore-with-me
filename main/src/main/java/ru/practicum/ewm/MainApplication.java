c807105db63e57b64cfebd093abea2835e9b09b2
Dinar Khazipov <khazipovdn@yandex.ru>
2025-04-27 01:07:15 +0300

    корректировка application.properties

--- main/src/main/java/ru/practicum/ewm/MainApplication.java
+++ main/src/main/java/ru/practicum/ewm/MainApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = {"ru.practicum.ewm"})
-@EnableJpaRepositories(basePackages = {"ru.practicum.ewm.repository"})
-@EntityScan(basePackages = {"ru.practicum.ewm.model"})
 public class MainApplication {
     public static void main(String[] args) {
         SpringApplication.run(MainApplication.class, args);
