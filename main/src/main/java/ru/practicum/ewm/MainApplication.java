75158e46b0c5a3ae9dfa8edf1724e22fb91af0d2
Dinar Khazipov <khazipovdn@yandex.ru>
2025-05-05 21:08:58 +0300

    с учетом замечаний ревьюера

--- main/src/main/java/ru/practicum/ewm/MainApplication.java
+++ main/src/main/java/ru/practicum/ewm/MainApplication.java
@@ -3,7 +3,7 @@ package ru.practicum.ewm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"ru.practicum.ewm"})
+@SpringBootApplication
 public class MainApplication {
     public static void main(String[] args) {
         SpringApplication.run(MainApplication.class, args);
