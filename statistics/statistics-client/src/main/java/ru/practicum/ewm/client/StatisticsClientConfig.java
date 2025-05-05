686ed56eff211769f7063da929c7e7db05f42c97
Dinar Khazipov <khazipovdn@yandex.ru>
2025-05-05 21:51:25 +0300

    с учетом дополнений2

--- statistics/statistics-client/src/main/java/ru/practicum/ewm/client/StatisticsClientConfig.java
+++
@@ -1,9 +0,0 @@
-package ru.practicum.ewm.client;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan("ru.practicum.ewm.client")
-public class StatisticsClientConfig {
-}
