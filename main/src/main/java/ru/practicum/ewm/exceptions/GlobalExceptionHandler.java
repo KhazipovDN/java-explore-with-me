a0e8aa2514fd57cefe46df53a7e09dace2b8cff8
Dinar Khazipov <khazipovdn@yandex.ru>
2025-05-08 19:56:55 +0300

    корректировка ошибок постмана

--- main/src/main/java/ru/practicum/ewm/exceptions/GlobalExceptionHandler.java
+++ main/src/main/java/ru/practicum/ewm/exceptions/GlobalExceptionHandler.java
@@ -4,6 +4,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -30,5 +32,12 @@ public class GlobalExceptionHandler {
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorResponse handleGenericException(final Exception e) {
         return new ErrorResponse("An unexpected error occurred: " + e.getMessage());
+    }
+
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleMissingServletRequestParameterException(MissingServletRequestParameterException ex) {
+        String message = String.format("Обязательный параметр '%s' отсутствует", ex.getParameterName());
+        return new ErrorResponse(message);
     }
 }
